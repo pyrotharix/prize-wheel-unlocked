85bf136f93cb6302cf6a12f5eab27efd0fa51407
Pyro <pyrotharix@gmail.com>
2025-05-28 09:30:15 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+// Сохраняем реферальный код из URL в localStorage
+const urlParams = new URLSearchParams(window.location.search);
+const refCode = urlParams.get('ref');
+if (refCode) {
+  localStorage.setItem('referral_code', refCode);
+}
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
